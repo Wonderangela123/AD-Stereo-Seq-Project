395b0c6560031b57ffac495cf6e6cf579ad04519
Wonderangela123 <140135188+Wonderangela123@users.noreply.github.com>
2023-07-27 15:34:54 -0500

    Create amyloid_tau_location.py

---
+++ locations of amyloid and tau/amyloid_tau_location.py
@@ -0,0 +1,15 @@
+# # Read the Stereoseq data through python and write the gene expression matrix into feather file and gene name into csv 
+import utils
+import pandas as pd
+import stereo as st
+import pyarrow.feather
+
+sample = "B01809C2"
+data_path = "/work/ygong/stereo_seq_public/{}/GeneExpMatrix/{}.cellbin.gef".format(sample, sample)
+st.io.read_gef_info(data_path)
+data1 = st.io.read_gef(file_path=data_path, bin_type='cell_bins')
+df = data1.to_df()
+df = df.T
+pyarrow.feather.write_feather(df, "/work/aliu10/AD_Stereoseq_Project/processed_data/{}_matrix.feather".format(sample))
+gene_list = pd.DataFrame(df.index)
+gene_list.to_csv("/work/aliu10/AD_Stereoseq_Project/processed_data/{}_gene_list.csv".format(sample))
