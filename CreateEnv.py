889e6d92d6392d934b8611e06a5c192b4eb1aecf
Wonderangela123 <140135188+Wonderangela123@users.noreply.github.com>
2023-08-01 14:57:01 -0500

    Update CreateEnv.py

--- CreateEnv.py
+++ CreateEnv.py
@@ -1,3 +1,4 @@
+## Python environment
 conda create -n ADstereo python=3.8
 conda activate ADstereo
 
@@ -7,3 +8,20 @@ cd stereopy
 python setup.py install
 
 conda install -c conda-forge notebook
+
+
+## R environment
+conda create -n ADstereo-R
+conda activate ADstereo-R
+
+conda install -c conda-forge r-base
+conda install -c conda-forge notebook
+conda install -c conda-forge r-irkernel  ## use R in notebook
+
+conda install -c conda-forge r-seurat
+
+## install "SingleR" in R
+if (!require("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+
+BiocManager::install("SingleR")
