a3b5ccc7ffa6556418bf28b352424658a2ad60b3
Wonderangela123 <140135188+Wonderangela123@users.noreply.github.com>
2023-08-10 15:11:33 -0500

    Update 3. Annotation_Marker_Genes.py

--- locations of amyloid and tau/3. Annotation_Marker_Genes.py
+++ locations of amyloid and tau/3. Annotation_Marker_Genes.py
@@ -26,3 +26,19 @@ data.plt.cluster_scatter(res_key='anno_cluster_leiden')
 
 ## check if the same cell types are close, e.g., "Ex.1" is close to "Ex.3"
 data.plt.umap(res_key='umap', cluster_key='anno_cluster_leiden')
+
+
+
+## gene expression regulation
+markers = pd.read_csv("/work/aliu10/AD_Stereoseq_Project/processed_data/B01809C2/Gene_markers.csv", sep=",", index_col=0)
+
+## microglia
+mic = markers.loc[:,["7_pvalues_adj", "7_log2fc", "16_pvalues_adj", "16_log2fc"]]  ## extract microglia (cluster 7 and 16)
+mic = mic.loc[mic.loc[:,"7_log2fc"] * mic.loc[:,"16_log2fc"] > 0] ## keep only the genes with the same expression regulation patterns in various clusters
+mic = mic.loc[mic.loc[:,"7_pvalues_adj"] < 0.05] ## set the singnificance level as 0.05
+mic = mic.loc[mic.loc[:,"16_pvalues_adj"] < 0.05]
+
+import numpy as np
+mic['regulation'] = np.where(mic['7_log2fc'] > 0, "Upregulation", "Downregulation")
+mic.iloc[:,-1].to_csv("/work/aliu10/AD_Stereoseq_Project/processed_data/B01809C2/gene_regulation_microglia.csv", sep = " ")
+
