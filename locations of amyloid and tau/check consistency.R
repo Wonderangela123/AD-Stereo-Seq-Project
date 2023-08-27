74f185c7b9288eb21dedf90d60872d79749d686e
Wonderangela123 <140135188+Wonderangela123@users.noreply.github.com>
2023-08-27 00:11:16 -0500

    Create check consistency.R

---
+++ locations of amyloid and tau/check consistency.R
@@ -0,0 +1,46 @@
+library(Seurat)
+library(SeuratDisk)
+
+setwd("/work/aliu10/AD_Stereoseq_Project/reference")
+data_pub <- LoadH5Seurat("integrated.h5Seurat")
+
+setwd("/work/aliu10/AD_Stereoseq_Project/processed_data/B01809C2/")
+Convert("B01809C2_extract.anndata.h5ad", dest = "h5seurat", overwrite = TRUE)
+data = LoadH5Seurat("B01809C2_extract.anndata.h5seurat")
+
+# identify variable features for each dataset independently
+my_list = list(data, data_pub)
+features <- SelectIntegrationFeatures(object.list = my_list)
+
+anchors <- FindIntegrationAnchors(object.list = my_list, anchor.features = features)
+# this command creates an 'integrated' data assay
+combined <- IntegrateData(anchorset = anchors)
+
+########################
+Scaling features for provided objects
+
+Finding all pairwise anchors
+
+Running CCA
+
+Merging objects
+
+Finding neighborhoods
+
+Finding anchors
+
+	Found 32521 anchors
+
+Filtering anchors
+
+	Retained 731 anchors
+
+Merging dataset 1 into 2
+
+Extracting anchors for merged samples
+
+Finding integration vectors
+
+Finding integration vector weights
+
+Integrating data
