952c14867e5d315eef8333a167698d26c2c4d752
Wonderangela123 <140135188+Wonderangela123@users.noreply.github.com>
2023-08-31 16:08:09 -0500

    Update check consistency between BA10 and public data.R

--- locations of amyloid and tau/check consistency between BA10 and public data.R
+++ locations of amyloid and tau/check consistency between BA10 and public data.R
@@ -5,13 +5,17 @@ setwd("/work/aliu10/AD_Stereoseq_Project/reference")
 data_pub <- LoadH5Seurat("integrated.h5Seurat")
 data_pub$region = "BA9"
 
-setwd("/work/aliu10/AD_Stereoseq_Project/processed_data/B01809C2/")
-Convert("B01809C2_extract.anndata.h5ad", dest = "h5seurat", overwrite = TRUE)
-data = LoadH5Seurat("B01809C2_extract.anndata.h5seurat")
-data$region = "BA10"
+Convert('/work/ygong/stereo_seq_public/MIT_AD.h5ad', dest = "h5seurat", overwrite = TRUE)
+data_MIT = LoadH5Seurat('/work/ygong/stereo_seq_public/MIT_AD.h5seurat')
+data_MIT$region = "BA10"
+
+data_MIT_neuron <- subset(data_MIT, subset = broad.cell.type %in% c("Ex", "In"))
+
+x <- NormalizeData(data_MIT_neuron)
+x <- FindVariableFeatures(x, selection.method = "vst", nfeatures = 2000)
 
 # identify variable features for each dataset independently
-my_list = list(data, data_pub)
+my_list = list(x, data_pub)
 features <- SelectIntegrationFeatures(object.list = my_list)
 
 anchors <- FindIntegrationAnchors(object.list = my_list, anchor.features = features)
@@ -22,6 +26,6 @@ combined <- ScaleData(combined, verbose = FALSE)
 combined <- RunPCA(combined, npcs = 30, verbose = FALSE)
 combined <- RunUMAP(combined, reduction = "pca", dims = 1:30)
 
-p1 <- DimPlot(combined, reduction = "umap", group.by = "BA10")
-p2 <- DimPlot(combined, reduction = "umap", label = TRUE, repel = TRUE)
-p1 + p2
+png("/work/aliu10/AD_Stereoseq_Project/reference/BA9vs10.png")
+DimPlot(combined, reduction = "umap", group.by = "region", raster=FALSE)
+dev.off() 
