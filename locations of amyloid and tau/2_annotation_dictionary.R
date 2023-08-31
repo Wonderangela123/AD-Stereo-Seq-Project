d62fd614232a43ea453607b1f84f991559a3fac0
Wonderangela123 <140135188+Wonderangela123@users.noreply.github.com>
2023-08-30 21:42:56 -0500

    Update 2_annotation_dictionary.R

--- locations of amyloid and tau/2_annotation_dictionary.R
+++ locations of amyloid and tau/2_annotation_dictionary.R
@@ -17,7 +17,7 @@ anno_dict_cell = SingleR(test, ref, labels=ref$broad.cell.type, assay.type.test=
 anno_dict_cluster$cluster = row.names(anno_dict_cluster)
 anno_dict_cluster$dict = paste(anno_dict_cluster$labels, anno_dict_cluster$cluster, sep = ".")
 
-write.table(anno_dict_cluster, file = '/work/aliu10/AD_Stereoseq_Project/processed_data/B01809C2/annotation_dict_cluster.txt')
+write.table(anno_dict_cluster, file = '/work/aliu10/AD_Stereoseq_Project/processed/annotation_dict_cluster.txt')
 
 
 
@@ -25,7 +25,7 @@ write.table(anno_dict_cluster, file = '/work/aliu10/AD_Stereoseq_Project/process
 # anno_dict_cell$cell = row.names(anno_dict_cell)
 # anno_dict_cell$dict = paste(anno_dict_cell$labels, anno_dict_cell$cell, sep = ".")
 
-# write.table(anno_dict_cell, file = '/work/aliu10/AD_Stereoseq_Project/processed_data/B01809C2/annotation_dict_cell.txt')
+# write.table(anno_dict_cell, file = '/work/aliu10/AD_Stereoseq_Project/processed/annotation_dict_cell.txt')
 
 
 
