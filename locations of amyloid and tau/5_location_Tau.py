97a79a126343a06dda5b2c107903a79c3c349c08
Wonderangela123 <140135188+Wonderangela123@users.noreply.github.com>
2023-09-11 21:30:31 -0500

    Update 5_location_Tau.py

--- locations of amyloid and tau/5_location_Tau.py
+++ locations of amyloid and tau/5_location_Tau.py
@@ -57,6 +57,14 @@ data.tl.annotation(
         res_key='anno_cluster_leiden' ## store annotation in "res_key" as a keyward
         )
 
+# save as stereodata
+st.io.write_h5ad(
+        data,
+        use_raw=True,
+        use_result=True,
+        output='/work/aliu10/AD_Stereoseq_Project/processed/data/B01809C2/GeneExpMatrix/B01809C2.stereo.h5ad',
+        )
+
 # Subset the cells annotated as "neuron"
 id = data.tl.result['anno_cluster_leiden']['group'].str.contains('Ex|In')
 id = id[id].index.tolist()
@@ -107,3 +115,31 @@ data1.tl.annotation(
         res_key='anno_cluster_leiden' ## store annotation in "res_key" as a keyward
         )
 
+# read stereodata
+data = st.io.read_stereo_h5ad(
+        file_path='/work/aliu10/AD_Stereoseq_Project/processed/data/B01809C2/GeneExpMatrix/B01809C2.stereo.h5ad',
+        use_raw=True,
+        use_result=True,
+        )
+
+# draw a plot with 'tau' location
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Extract cell names of tau from data1
+tau = data1.cells.cell_name[data1.tl.result['anno_cluster_leiden']['group'].str.contains('cases')]
+
+# Convert the cell names from 'data' into a pandas series
+cell_names_series = pd.Series(data.cells.cell_name)
+
+# Identify which cells in 'data' match the cell names in 'tau'
+id_tau = cell_names_series.isin(tau)
+
+# If match then 'red' otherwise 'grey'
+data.cells['tau'] = ['red' if cell else 'gray' for cell in id_tau]
+data.cells['tau'] = data.cells['tau'].astype('category')
+
+# plot the cells by 'tau' (red)
+fig, ax = plt.subplots(figsize=(10, 10))
+plt.scatter(data.position[:,0], data.position[:,1], c=data.cells['tau'], s=10)
+ax.invert_yaxis() # change the direction of y axis
