97e9fc0cfbe2a9df733be27c2cda5a9fd933f0d3
Wonderangela123 <140135188+Wonderangela123@users.noreply.github.com>
2023-09-15 18:26:41 -0500

    Update 5_location_Tau.py

--- locations of amyloid and tau/5_location_Tau.py
+++ locations of amyloid and tau/5_location_Tau.py
@@ -1,68 +1,12 @@
 import stereo as st
 import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
 
-data = st.io.read_ann_h5ad(
-       file_path='/work/aliu10/AD_Stereoseq_Project/processed/data/integrated.h5ad',
-       spatial_key=None,
-       bin_type="cell_bins"
-       )
-
-data.tl.filter_cells(
-        min_gene=20, 
-        min_n_genes_by_counts=200,
-        max_n_genes_by_counts=4000, 
-        pct_counts_mt=10,
-        inplace=True
-        )
-
-data.tl.raw_checkpoint() 
-
-data.tl.quantile()
-
-data.tl.highly_variable_genes(
-            min_mean=0.0125,
-            max_mean=3,
-            min_disp=0.5,
-            n_top_genes=2000,
-            res_key='highly_variable_genes'
-            )
-
-data.tl.scale() # Scale each gene to unit variance. Clip values exceeding standard deviation 10. 
-
-data.tl.pca(
-        use_highly_genes=True,
-        n_pcs=30,
-        res_key='pca'
-        )
-
-data.tl.neighbors(pca_res_key='pca', res_key='neighbors')
-
-data.tl.umap(
-        pca_res_key='pca',
-        neighbors_res_key='neighbors',
-        res_key='umap'
-        )
-
-data.tl.leiden(neighbors_res_key='neighbors',res_key='leiden', resolution=0.1)
-
-# read text file into pandas DataFrame
-anno_dict_cluster = pd.read_csv("/work/aliu10/AD_Stereoseq_Project/processed/data/annotation_dict_cluster.txt", sep=" ")
-
-# convert annotation dictionary into list
-annotation_dict_cluster = anno_dict_cluster.dict.tolist()
-
-data.tl.annotation(
-        annotation_information=annotation_dict_cluster, ## annotation dictionary
-        cluster_res_key='leiden',
-        res_key='anno_cluster_leiden' ## store annotation in "res_key" as a keyward
-        )
-
-# save as stereodata
-st.io.write_h5ad(
-        data,
+data = st.io.read_stereo_h5ad(
+        file_path='/work/aliu10/AD_Stereoseq_Project/processed/data/B01809C2/GeneExpMatrix/B01809C2.stereo.h5ad',
         use_raw=True,
         use_result=True,
-        output='/work/aliu10/AD_Stereoseq_Project/processed/data/B01809C2/GeneExpMatrix/B01809C2.stereo.h5ad',
         )
 
 # Subset the cells annotated as "neuron"
@@ -72,6 +16,7 @@ id = id[id].index.tolist()
 data1 = data
 data1.cells.cell_name = data.cells.cell_name[id]
 data1.exp_matrix = data.exp_matrix[id]
+data1.position = data.position[id, :]
 
 data1.tl.raw_checkpoint() 
 
@@ -101,10 +46,13 @@ data1.tl.umap(
         res_key='umap'
         )
 
-data1.tl.leiden(neighbors_res_key='neighbors',res_key='leiden', resolution=0.1)
+data1.tl.leiden(neighbors_res_key='neighbors',res_key='leiden', resolution=0.5)
+
+# After getting annotation dictionary
+import pandas as pd
 
 # read text file into pandas DataFrame
-anno_dict_cluster = pd.read_csv("/work/aliu10/AD_Stereoseq_Project/processed/data/annotation_dict_subtype.txt", sep=" ")
+anno_dict_cluster = pd.read_csv("/work/aliu10/AD_Stereoseq_Project/processed/data/B01809C2/GeneExpMatrix/B01809C2_annotation_dict_subtype.txt", sep=" ")
 
 # convert annotation dictionary into list
 annotation_dict_cluster = anno_dict_cluster.dict.tolist()
@@ -112,19 +60,16 @@ annotation_dict_cluster = anno_dict_cluster.dict.tolist()
 data1.tl.annotation(
         annotation_information=annotation_dict_cluster, ## annotation dictionary
         cluster_res_key='leiden',
-        res_key='anno_cluster_leiden' ## store annotation in "res_key" as a keyward
+        res_key='anno_cluster_leiden' ## store annotation in "res_key" as a keyword
         )
 
-# read stereodata
+#######################################################
+# data will be covered, so we have to load data again.
 data = st.io.read_stereo_h5ad(
         file_path='/work/aliu10/AD_Stereoseq_Project/processed/data/B01809C2/GeneExpMatrix/B01809C2.stereo.h5ad',
         use_raw=True,
         use_result=True,
         )
-
-# draw a plot with 'tau' location
-import pandas as pd
-import matplotlib.pyplot as plt
 
 # Extract cell names of tau from data1
 tau = data1.cells.cell_name[data1.tl.result['anno_cluster_leiden']['group'].str.contains('cases')]
@@ -141,5 +86,14 @@ data.cells['tau'] = data.cells['tau'].astype('category')
 
 # plot the cells by 'tau' (red)
 fig, ax = plt.subplots(figsize=(10, 10))
-plt.scatter(data.position[:,0], data.position[:,1], c=data.cells['tau'], s=10)
+plt.scatter(data.position[:,0], data.position[:,1], c=data.cells['tau'], s=5)
+
+legend_elements = [Line2D([0], [0], marker='o', color='w', label='Tau', markersize=10, markerfacecolor='red'),
+                   Line2D([0], [0], marker='o', color='w', label='Others', markersize=10, markerfacecolor='grey')]
+
+# Add the legend to the plot
+ax.legend(handles=legend_elements, fontsize=14)
+
 ax.invert_yaxis() # change the direction of y axis
+
+fig.savefig("/work/aliu10/AD_Stereoseq_Project/processed/data/B01809C2/GeneExpMatrix/B01809C2.png")
