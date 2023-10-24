ecc956af5d059d98e9b0b9c88aafb89ce446b95b
Wonderangela123 <140135188+Wonderangela123@users.noreply.github.com>
2023-10-24 13:15:03 -0500

    Update CreateEnv.py

--- CreateEnv.py
+++ CreateEnv.py
@@ -1,6 +1,6 @@
 ## Python environment
-conda create -n st python=3.8
-conda activate st
+conda create -n stereo python=3.8
+conda activate stereo
 
 ## Development version
 git clone -b dev https://github.com/STOmics/stereopy.git
